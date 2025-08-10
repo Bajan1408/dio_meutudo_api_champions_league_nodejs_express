0c10ea13fa8afd5728a8c6266de2df0d67b761d1
Bajan1408 <gianrodrigues@gmail.com>
2025-08-09 22:22:41 -0300

    feat: reading json, cors, simple frontend for tests cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express, { json } from 'express';
 import router from './routes';
+import cors from 'cors';
 
 export function createApp() {
     const app = express();
+
+    //incluído o endereço http://127.0.0.1:550 no corsOptions para permitir acessar a api via browser com o live server do VsCode
+    const corsOptions = {
+        origin: ['http://localhost:3335', 'http://127.0.0.1:5500'],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE']
+    }
+
+    app.use(cors(corsOptions));
 
     app.use(json());
 
